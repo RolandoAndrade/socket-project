96ab486ed081768b5feb3409f5403b257670b8dd
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-11-09 17:29:07 -0400

    Defining application domain

---
+++ src/messages/domain/message.repository.ts
@@ -0,0 +1,13 @@
+import {HelloResponse} from "../../responses/domain/hello-response";
+import {LengthResponse} from "../../responses/domain/length-response";
+import {MessageResponse} from "../../responses/domain/message-response";
+import {ChecksumResponse} from "../../responses/domain/checksum-response";
+import {ByeResponse} from "../../responses/domain/bye-response";
+
+export interface MessageRepository{
+    sendHello(username: string): HelloResponse;
+    getMessageLength(): LengthResponse;
+    getMessage(udpPort: number): MessageResponse;
+    checksum(md5message: string): ChecksumResponse;
+    sendBye(): ByeResponse;
+}
