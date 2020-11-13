2bc32ac535d78dbe82ce07bea8b46b460334552a
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-11-13 08:29:36 -0400

    UDP client - decode message to ascii

--- apps/server/src/messages/infrastucture/messages.socket.repository.ts
+++ apps/server/src/messages/infrastucture/messages.socket.repository.ts
@@ -13,9 +13,10 @@ export class MessagesSocketRepository implements MessageRepository {
     constructor(private readonly socket: Socket, private readonly eventBus: EventBus) {
         this.socket.on("data", (data) => eventBus.publish(EventBusMessages.MESSAGE_RECEIVED, data.toString()));
         this.udpClient = createSocket("udp4");
-
-        this.udpClient.on("msg", (data) => {
-            this.eventBus.publish(EventBusMessages.MESSAGE_RECEIVED, data.toString());
+        this.udpClient.on("message", (data) => {
+            const buffer = Buffer.from(data.toString(), 'base64');
+            const decodedMessage = buffer.toString('ascii')
+            this.eventBus.publish(EventBusMessages.MESSAGE_RECEIVED, decodedMessage);
             this.udpClient.close();
         })
     }
@@ -25,7 +26,7 @@ export class MessagesSocketRepository implements MessageRepository {
     async getMessage(udpPort: number): Promise<void> {
         this.udpClient.bind(udpPort, async ()=>{
             await new Promise((resolve, reject) => {
-                this.socket.write(`givememsg ${'55611'}`, (error)=>{
+                this.socket.write(`givememsg ${this.udpClient.address().port}`, (error)=>{
                     if(error){
                         this.eventBus.publish(EventBusMessages.MESSAGE_RECEIVED, error.message)
                     }
@@ -43,7 +44,6 @@ export class MessagesSocketRepository implements MessageRepository {
         await new Promise((resolve, reject) => {
             this.socket.write(`helloiam ${username}`, (error)=>{
                 if(error){
-                    console.log("emahhe")
                     this.eventBus.publish(EventBusMessages.MESSAGE_RECEIVED, error.message)
                 }
             });
